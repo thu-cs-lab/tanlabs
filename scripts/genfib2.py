4b20df51e8d09b05af96a916db2e73dca06642a3
Wende Tan <twd2@163.com>
2023-11-11 16:53:06 +0800

    fib_shuffled: fix nexthop ips

--- scripts/genfib2.py
+++ scripts/genfib2.py
@@ -6,9 +6,12 @@ import sys
 # Usage:
 #   python3 genfib2.py fib_shuffled.txt fib_shuffled2.txt
 
+nexthop_ips = [f'fe80::8e1f:64ff:fe69:100{i + 1}' for i in range(4)]
+
 with open(sys.argv[1], 'r') as fin, \
      open(sys.argv[2], 'w') as fout:
     for l in fin:
         net, prefix_len, nexthop_ip, nexthop_iface = l.strip().split()
         nexthop_iface = random.randint(0, 3)
+        nexthop_ip = nexthop_ips[nexthop_iface]
         fout.write(f'{net} {prefix_len} {nexthop_ip} {nexthop_iface}\n')
